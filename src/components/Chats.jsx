240222eef37681c4ba62fb5e3d99c7c68dc36fe8
s-yordanova <sisito0606@abv.bg>
2022-09-22 14:51:08 +0300

    chat and messages done

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,38 +1,53 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 const Chats = () => {
-  return (
-    <div className='chats'>
-      <div className="userChat">
-        <img src="https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-733872.jpg&fm=jpg" alt="" />
-        <div className="userChatInfo">
-          <span>Tonya</span>
-          <p>Let's go to eat</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img src="https://st3.depositphotos.com/4487369/13149/i/600/depositphotos_131491354-stock-photo-portrait-of-beautiful-girl-on.jpg" alt="" />
-        <div className="userChatInfo">
-          <span>Sisi</span>
-          <p>What are you doin?</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img src="https://media.istockphoto.com/photos/depressed-girl-at-home-picture-id1165709528?k=20&m=1165709528&s=170667a&w=0&h=cIbwcyOjr0m4lWmTFUn8kql7WEc-QkSimkYljCWVlLU=" alt="" />
-        <div className="userChatInfo">
-          <span>Anna</span>
-          <p>How are you?</p>
-        </div>
-      </div>
-    </div>
-  )
-}
+  const [chats, setChats] = useState([]);
 
-export default Chats
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  const handleSelect = (u) => {
+    dispatch({ type: "CHANGE_USER", payload: u });
+  };
+
+  return (
+    <div className="chats">
+      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+        <div
+          className="userChat"
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
+          <img src={chat[1].userInfo.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].lastMessage?.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Chats;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,21 +1,39 @@
 import React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div className="message owner">
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
       <div className="messageInfo">
         <img
-          src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-733872.jpg&fm=jpg"
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
           alt=""
         />
         <span>just now</span>
       </div>
       <div className="messageContent">
-        <p>hello</p>
-        <img
-          src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-733872.jpg&fm=jpg"
-          alt=""
-        /> 
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
   );
